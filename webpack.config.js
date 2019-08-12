c520dba009f0384938cc2005bf8eeda850bb3c1c
Denis Bendrikov <Denis Bendrikov@gmail.com>
2019-08-12 19:26:41 +0300

    feat(typescript): migrate to typescript and rxjs@6
    Migrate to typescript and refactor to use rxjs@6.
    Update demo directory.
    Update module entry and types fields.
    Update docs.
    
    BREAKING CHANGE: use ES2015 exports instead of commonjs. UMD bundle now creates `window.ac` variable.

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,31 @@
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpackRxjsExternals = require('webpack-rxjs-externals');
 
 module.exports = {
-    output: {
-        filename: 'activity-checker.js',
-        library: 'ActivityChecker',
-        libraryTarget: 'umd'
-    },
+  output: {
+    filename: 'activity-checker.umd.js',
+    library: 'ac',
+    libraryTarget: 'umd'
+  },
 
-    externals: {
-        'rx-lite': 'Rx',
-    },
+  externals: [
+    webpackRxjsExternals()
+  ],
 
-    devtool: 'source-map',
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
 
-    plugins: [
-        new CleanWebpackPlugin()
+  module: {
+    rules: [
+      { test: /\.ts$/, loader: 'ts-loader' }
     ]
+  },
+
+  devtool: 'source-map',
+
+  plugins: [
+    new CleanWebpackPlugin()
+  ]
 };
 
